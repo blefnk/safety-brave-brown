174bfcd1a1d2b2e670da6c60b7719b7d92343fd6
Nazar Kornienko <blefnk@gmail.com>
2024-12-14 01:26:21 +0100

    Initial commit by @reliverse/cli

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+export const app = {
+  shortName: "Relivator",
+  name: "Relivator – The Next.js Template for Your eCommerce – by Bleverse",
+  url: "https://safety-brave-brown.vercel.app",
+  ogImage: "https://safety-brave-brown.vercel.app/og.jpg",
+  description:
+    "Relivator is the Next.js template making it easy to build the powerful eCommerce platforms.",
+  links: {
+    twitter: "https://twitter.com/reliverse_org",
+    github: "https://github.com/reliverse",
+  },
+};
+
+export type AppConfig = typeof app;
+
+export const META_THEME_COLORS = {
+  light: "#ffffff",
+  dark: "#09090b",
+};
